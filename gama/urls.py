728d5a4027934b7b95a9ca4d45c1418de0e41847
pruizf <pabloruizfabo@gmail.com>
2025-08-12 21:49:01 -0300

    reorders urls.py

--- gama/urls.py
+++ gama/urls.py
@@ -6,10 +6,10 @@ app_name = "gama"
 urlpatterns = [
     path("", views.index, name="index"),
     path("analysis_do", views.analysis_do, name="analysis_do"),
+    path("analysis/", views.analysis_show, name="analysis_show"),
+    path('analysis_bulk/', views.analysis_bulk, name='analysis_bulk'),
     path("error/<str:errtype>/", views.error, name="error"),
     path("export_results/", views.export_results, name="export_results"),
-    path("about/", views.about, name="about"),
-    path("analysis/", views.analysis_show, name="analysis_show"),
     path('clear/', views.clear_session, name='clear_session'),
-    path('analysis_bulk/', views.analysis_bulk, name='analysis_bulk'),
+    path("about/", views.about, name="about"),
 ]
